af227c8f05ac40cb70a5224a31cefcf31a93b294
ihm <ihm@debian8-ihm>
2016-01-08 15:44:43 +0100

    adrien.villoz@master.hes-so.ch Correction couleur out of range

--- CTempGauge/CTempGauge_paint.cpp
+++ CTempGauge/CTempGauge_paint.cpp
@@ -19,13 +19,14 @@ void CTempGauge::paint(QPainter* pPainter, QPaintEvent* pEvent)
     pPainter->fillRect(rect(), QBrush(Qt::red));
     */
     pPainter->fillRect(rect(), QBrush(Qt::black));
-    if(temp_ < 90){
-        pPainter->setBrush(QBrush(QColor::fromRgbF(((temp_-60)/30.),1, 0, 1)));
-    }
-    else{
-        pPainter->setBrush(QBrush(QColor::fromRgbF(1,1-((temp_-90)/30.), 0, 1)));
-    }
+
     if(temp_ > 60){
+        if(temp_ < 90){
+            pPainter->setBrush(QBrush(QColor::fromRgbF(((temp_-60)/30.),1, 0, 1)));
+        }
+        else{
+            pPainter->setBrush(QBrush(QColor::fromRgbF(1,1-((temp_-90)/30.), 0, 1)));
+        }
         pPainter->drawPie(rect(), 150*16, -(temp_-60)*16*2);
     }
     pPainter->setBrush(QBrush(Qt::white));
